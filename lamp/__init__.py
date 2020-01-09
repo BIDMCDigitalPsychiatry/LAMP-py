2a768cfbdd386b056f0f8c1c7ac45cbcd1602911
Your Name <ryan-hays@github.com>
2020-01-09 20:17:51 +0000

    All the new stuff

--- lamp/__init__.py
+++ lamp/__init__.py
@@ -4,28 +4,61 @@ from subject import Subject
 from cohort import Cohort
 from survey_question_dict import SurveyQuestionDict
 
-HOST = "https://api.lamp.digital"
-KEY = LOGIN_CREDENTIALS # your login credentials go here
+# import apis into sdk package
+from openapi_client.api.activity_api import ActivityApi
+from openapi_client.api.activity_spec_api import ActivitySpecApi
+from openapi_client.api.credential_api import CredentialApi
+from openapi_client.api.participant_api import ParticipantApi
+from openapi_client.api.researcher_api import ResearcherApi
+from openapi_client.api.result_event_api import ResultEventApi
+from openapi_client.api.sensor_event_api import SensorEventApi
+from openapi_client.api.sensor_spec_api import SensorSpecApi
+from openapi_client.api.study_api import StudyApi
+from openapi_client.api.type_api import TypeApi
 
-configuration = openapi_client.Configuration()
+
+# import ApiClient
+from openapi_client.api_client import ApiClient
+from openapi_client.configuration import Configuration
+from openapi_client.exceptions import OpenApiException
+from openapi_client.exceptions import ApiTypeError
+from openapi_client.exceptions import ApiValueError
+from openapi_client.exceptions import ApiKeyError
+from openapi_client.exceptions import ApiException
+# import models into sdk package
+from openapi_client.models.access_citation import AccessCitation
+from openapi_client.models.activity import Activity
+from openapi_client.models.activity_spec import ActivitySpec
+from openapi_client.models.credential import Credential
+from openapi_client.models.document import Document
+from openapi_client.models.duration_interval import DurationInterval
+from openapi_client.models.duration_interval_legacy import DurationIntervalLegacy
+from openapi_client.models.dynamic_attachment import DynamicAttachment
+from openapi_client.models.error import Error
+from openapi_client.models.metadata import Metadata
+from openapi_client.models.participant import Participant
+from openapi_client.models.researcher import Researcher
+from openapi_client.models.result_event import ResultEvent
+from openapi_client.models.sensor_event import SensorEvent
+from openapi_client.models.sensor_spec import SensorSpec
+from openapi_client.models.study import Study
+from openapi_client.models.temporal_event import TemporalEvent
+
+HOST = "https://api.lamp.digital"
+KEY = 'admin:LAMPLAMP' # your login credentials go here
+
+configuration = Configuration()
 configuration.host = HOST
 configuration.api_key['Authorization'] = KEY
 configuration.api_key_prefix['Authorization'] = 'Basic'
 
 
-activity = openapi_client.ActivityApi(openapi_client.ApiClient(configuration))
-activity_spec = openapi_client.ActivitySpecApi(openapi_client.ApiClient(configuration))
-environment_event = openapi_client.EnvironmentEventApi(openapi_client.ApiClient(configuration))
-fitness_event = openapi_client.FitnessEventApi(openapi_client.ApiClient(configuration))
-metadata_event = openapi_client.MetadataEventApi(openapi_client.ApiClient(configuration))
-participant = openapi_client.ParticipantApi(openapi_client.ApiClient(configuration))
-researcher = openapi_client.ResearcherApi(openapi_client.ApiClient(configuration))
-result_event = openapi_client.ResultEventApi(openapi_client.ApiClient(configuration))
-sensor_event = openapi_client.SensorEventApi(openapi_client.ApiClient(configuration))
-study = openapi_client.StudyApi(openapi_client.ApiClient(configuration))
-type = openapi_client.TypeApi(openapi_client.ApiClient(configuration))
-type_legacy = openapi_client.TypeLegacyApi(openapi_client.ApiClient(configuration))
-
-#subject_data = SubjectDataframe
-
-
+activity = ActivityApi(ApiClient(configuration))
+activity_spec = ActivitySpecApi(ApiClient(configuration))
+credential = CredentialApi(ApiClient(configuration))
+participant = ParticipantApi(ApiClient(configuration))
+researcher = ResearcherApi(ApiClient(configuration))
+result_event = ResultEventApi(ApiClient(configuration))
+sensor_event = SensorEventApi(ApiClient(configuration))
+study = StudyApi(ApiClient(configuration))
+type = TypeApi(ApiClient(configuration))
--- lamp/openapi_client/api/__init__.py
+++ lamp/openapi_client/api/__init__.py
@@ -5,13 +5,11 @@ from __future__ import absolute_import
 # import apis into api package
 from openapi_client.api.activity_api import ActivityApi
 from openapi_client.api.activity_spec_api import ActivitySpecApi
-from openapi_client.api.environment_event_api import EnvironmentEventApi
-from openapi_client.api.fitness_event_api import FitnessEventApi
-from openapi_client.api.metadata_event_api import MetadataEventApi
+from openapi_client.api.credential_api import CredentialApi
 from openapi_client.api.participant_api import ParticipantApi
 from openapi_client.api.researcher_api import ResearcherApi
 from openapi_client.api.result_event_api import ResultEventApi
 from openapi_client.api.sensor_event_api import SensorEventApi
+from openapi_client.api.sensor_spec_api import SensorSpecApi
 from openapi_client.api.study_api import StudyApi
 from openapi_client.api.type_api import TypeApi
-from openapi_client.api.type_legacy_api import TypeLegacyApi
